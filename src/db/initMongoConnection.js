8066a39a9f2abd3970c889ceafc228729bb3e1cb
Sabina KAlchuk <ssabki229@gmail.com>
2025-09-14 14:32:07 +0200

    fix initMango

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,23 +1,24 @@
+// src/db/initMongoConnection.js
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const initMongoConnection = async () => {
-  const {
-    MONGODB_USER,
-    MONGODB_PASSWORD,
-    MONGODB_HOST,
-    MONGODB_DB_NAME,
-  } = process.env;
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_DB_NAME } = process.env;
 
-  const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+  if (!MONGODB_USER || !MONGODB_PASSWORD || !MONGODB_HOST || !MONGODB_DB_NAME) {
+    console.error("❌ Missing required MongoDB environment variables!");
+    process.exit(1);
+  }
+
+  const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DB_NAME}?retryWrites=true&w=majority`;
 
   try {
     await mongoose.connect(connectionString);
-    console.log("✅ Database connection successful");
+    console.log("✅ Mongo connection successfully established!");
   } catch (error) {
-    console.error("❌ Database connection error:", error);
+    console.error("❌ Mongo connection error:", error);
     process.exit(1);
   }
 };
